50e53d3ee393b7ce6f7899d1837c9062652254a5
cjm7433 <cjm7433@psu.edu>
2025-10-30 12:51:31 -0400

    Lesson 4 progress

--- noki/snake_game/src/snake_game.adb
+++ noki/snake_game/src/snake_game.adb
@@ -1,26 +1,40 @@
 with Noki;
+with GNAT.OS_Lib;
+
 
 procedure Snake_Game is
-
-type Game_State_T is (Welcome, Play, Game_Over, Undefined);
-Game_State : Game_State_T := Welcome;
+  package N renames Noki;
+  use N;
+  type Game_State_T is (Welcome, Play, Game_Over, Undefined);
+  Game_State : Game_State_T := Welcome;
+  Input_Task : N.Input_Task_T;
 
 begin
-   loop
-      Noki.Log(Noki.Clear_Screen);
-      case Game_State is
-         when Welcome =>
-            Noki.Log("Welcome to Snakotron!");
-            Game_State := Play;
-         when Play =>
-            Noki.log("We are playing");
-            Game_State := Game_Over;
-         when Game_Over =>
-            Noki.Log("Game Over!");
-            Game_State := Undefined;
-         when others =>
-            Noki.Log("Press Ctrl+C to aport program!");
-         end case;
-      delay 1.0;
-   end loop;
+    loop
+      exit when Input_Cmd.Get = Quit;
+       Log(N.Clear_Screen);
+       case Game_State is
+          when Welcome =>
+            Log("Welcome to Snakotron!");
+            Log("Press Enter/Space to play, Q/Esc to quit");
+            if Input_Cmd.Get = Enter then
+              Game_State := Play;
+              Input_Cmd.Reset;
+            end if;
+          when Play =>
+             Log("We are playing");
+             Game_State := Game_Over;
+          when Game_Over =>
+            Log("Game Over!");
+            Log("Press Enter/Space to replay");
+            if Input_Cmd.Get = Enter then
+              Game_State := Play;
+              Input_Cmd.Reset;
+            end if;
+          when others =>
+            null;
+          end case;
+        delay 1.0;
+      end loop;
+      GNAT.OS_Lib.OS_Exit(0);
 end Snake_Game;
