9effda6aebeda11f75605ea777c40080a52706d4
moorec43 <moorec43@gmail.com>
2025-10-30 07:04:37 -0400

    Lesson 3 progress

--- noki/src/noki.ads
+++ noki/src/noki.ads
@@ -1,3 +1,7 @@
 package Noki is
+
+   CSI : constant String := Character'Val (16#1B#) & '[';
+   function Clear_Screen return String is (CSI & "2J" & CSI & "H");
    procedure Log (S : String);
+
 end Noki;
